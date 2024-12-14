7388bbdffa95885e60bbfe1cf8e8944355dd4fa0
d0ubt <sepabonn@unal.edu.co>
2024-12-14 10:18:16 -0500

    Agregando comentarios y limpiar recursos p3

--- p1_p2.c
+++ p1_p2.c
@@ -72,6 +72,7 @@ void limpiar_recursos(int fifo_1, int fifo_2, int shm_fd, int creador_semaforos)
 }
 
 int main(int argc, char *argv[]) {
+    //Verificar cantida argumentos
     if (argc != 4) {
         perror("Uso: p1 N a1 a2");
         exit(1);
@@ -81,16 +82,19 @@ int main(int argc, char *argv[]) {
     int a1 = atoi(argv[2]);
     int a2 = atoi(argv[3]);
 
+    //Verificas par e impar
     if (n < 1 || a1 % 2 != 0 || a2 % 2 == 0) {
-        perror("Valores incorrectos");
+        perror("N>1 , a1: par , a2: impar");
         exit(1);
     }
 
+    //Abrir semaforo 3 sirve para saber si se ejecuto anteriormente correctamente el P3
     sem_3 = sem_open(SEM_3_NAME, 0);
     if (sem_3 == SEM_FAILED) {
         perror("P3 no esta en ejecucion");
         exit(1);
     }
+    //Abrir semaforo4
     sem_4 = sem_open(SEM_4_NAME, 0);
     if (sem_4 == SEM_FAILED) {
         perror("Error al abrir semáforo SEM_4");
@@ -98,21 +102,26 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
+    //Borrar semaforo 1 y 2
     sem_unlink(SEM_1_NAME);
     sem_unlink(SEM_2_NAME);
 
+    //Aleatoriedad
     srand(time(NULL));
     int boolean = rand() % 2;
 
+    //Crear semaforo 1 y 2 
     sem_1 = sem_open(SEM_1_NAME, O_CREAT, 0666, boolean == 0 ? 1 : 0);
     sem_2 = sem_open(SEM_2_NAME, O_CREAT, 0666, boolean == 0 ? 0 : 1);
 
+    //Verificar creacion
     if (sem_1 == SEM_FAILED || sem_2 == SEM_FAILED) {
         perror("Error al crear semáforos SEM_1 o SEM_2");
         limpiar_recursos(-1, -1, -1, 0);
         exit(EXIT_FAILURE);
     }
 
+    //Abriendo memoria compartida
     int shm_fd = shm_open(SHM_NAME, O_RDWR, 0666);
     if (shm_fd < 0) {
         perror("Error al abrir memoria compartida");
@@ -120,6 +129,7 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
+    //Puntero memoria compartida
     int *shm_ptr = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
     if (shm_ptr == MAP_FAILED) {
         perror("Error al mapear memoria compartida");
@@ -127,6 +137,7 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
+    //Abriendo tuberia 1 y 2
     int fifo_1 = open(FIFO_1, O_RDONLY);
     int fifo_2 = open(FIFO_2, O_RDONLY);
 
@@ -136,6 +147,7 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
+    //Creacion proceso hijo despues de abrir todos los recursos necesarios
     pid_t pid = fork();
 
     if (pid < 0) {
@@ -166,14 +178,13 @@ int main(int argc, char *argv[]) {
         int senal;
         read(fifo_1, &senal, sizeof(senal));
 
-        limpiar_recursos(fifo_1, fifo_2, shm_fd, 1);
 
         if (senal == -3) {
             printf("-3 P1 termina\n");
         }
 
         wait(NULL);
-
+        limpiar_recursos(fifo_1, fifo_2, shm_fd, 1);
         if (senal != -3){
             perror("P1 Fallo diferente a -3");
             exit(1);
--- p3.c
+++ p3.c
@@ -16,66 +16,95 @@
 sem_t *sem_3;
 sem_t *sem_4;
 
+void limpiar_recursos(int fifo_1, int fifo_2, int shm, int creador_semaforos) {
+    if (fifo_1 >= 0) close(fifo_1);
+    if (fifo_2 >= 0) close(fifo_2);
+    if (shm >= 0) close(shm);
+
+    if (sem_3) sem_close(sem_3);
+    if (sem_4) sem_close(sem_4);
+
+    if (creador_semaforos) {
+        sem_unlink(SEM_3_NAME);
+        sem_unlink(SEM_4_NAME);
+    }
+
+    shm_unlink(SHM_NAME);
+    unlink(FIFO_1);
+    unlink(FIFO_2);
+}
+
 int main() {
-    
-    // Crear memoria compartida
+
+    //Crear memoria
     shm_unlink(SHM_NAME);
     int shm = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
     if (shm == -1) {
         perror("shm_open");
         exit(EXIT_FAILURE);
     }
+    //Tamano memoria
     if (ftruncate(shm, sizeof(int)) == -1) {
         perror("ftruncate");
+        limpiar_recursos(-1, -1, shm, 0);
         exit(EXIT_FAILURE);
     }
+    //Puntero memoria
     int *shm_ptr = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, shm, 0);
     if (shm_ptr == MAP_FAILED) {
         perror("mmap");
+        limpiar_recursos(-1, -1, shm, 0);
         exit(EXIT_FAILURE);
     }
 
-    //Eliminar semaforo 3 y 4
+    //Borrar semaforos anteriores por si acaso
     sem_unlink(SEM_3_NAME);
     sem_unlink(SEM_4_NAME);
-    
-    //Crear semaforo 3
+
+    //Abrir semaforo 3
     sem_3 = sem_open(SEM_3_NAME, O_CREAT, 0666, 1);
     if (sem_3 == SEM_FAILED) {
         perror("sem_open sem_3");
+        limpiar_recursos(-1, -1, shm, 0);
         exit(EXIT_FAILURE);
     }
-    //Crear semaforo 4
+    //Abrir semaforo  4
     sem_4 = sem_open(SEM_4_NAME, O_CREAT, 0666, 0);
     if (sem_4 == SEM_FAILED) {
         perror("sem_open sem_4");
+        limpiar_recursos(-1, -1, shm, 1);
         exit(EXIT_FAILURE);
     }
 
-    // Eliminando tuberías por si acaso
+    //Borrar tuberias anteriores por si acaso
     unlink(FIFO_1);
     unlink(FIFO_2);
-    
-    // Creando tuberías y verificando
+
+    //Crear tuberia 1
     if (mkfifo(FIFO_1, 0666) == -1) {
         perror("mkfifo FIFO_1");
+        limpiar_recursos(-1, -1, shm, 1);
         exit(EXIT_FAILURE);
     }
+    //Crear tuberia 2
     if (mkfifo(FIFO_2, 0666) == -1) {
         perror("mkfifo FIFO_2");
+        limpiar_recursos(-1, -1, shm, 1);
         exit(EXIT_FAILURE);
     }
 
-    // Abriendo tuberías
-    int fifo_1 = open(FIFO_1,O_WRONLY);
+    //Abrir tuberia 1 en mood escritura
+    int fifo_1 = open(FIFO_1, O_WRONLY);
     if (fifo_1 == -1) {
         perror("open FIFO_1");
+        limpiar_recursos(-1, -1, shm, 1);
         exit(EXIT_FAILURE);
     }
-
+    //Abrir tuberia 2 en modo escritura
     int fifo_2 = open(FIFO_2, O_WRONLY);
     if (fifo_2 == -1) {
         perror("open FIFO_2");
+        limpiar_recursos(fifo_1, -1, shm, 1);
         exit(EXIT_FAILURE);
     }
 
@@ -83,9 +112,11 @@ int main() {
 
     int finalizados = 0;
     int senal = -3;
-     
+
+    //Ciclo de espera y lectura a P1_P2
+
     while (finalizados < 2) {
-        sem_wait(sem_4); 
+        sem_wait(sem_4);
         int valor = *shm_ptr;
         printf("%d ", valor);
 
@@ -95,18 +126,15 @@ int main() {
         } else if (valor == -2) {
             write(fifo_2, &senal, sizeof(int));
             finalizados++;
-        } 
+        }
         sem_post(sem_3);
     }
+
     printf("\n");
 
-    // Liberar recursos
+    //Borrando recursos creados
     munmap(shm_ptr, sizeof(int));
-    sem_close(sem_3);
-    sem_unlink(SEM_3_NAME);
-    shm_unlink(SHM_NAME);
-    unlink(FIFO_1);
-    unlink(FIFO_2);
+    limpiar_recursos(fifo_1, fifo_2, shm, 1);
 
     return 0;
 }
